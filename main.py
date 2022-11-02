0c9e3e932b6a711897ac341d81762f0539c9eef1
liushooter <shooter.hero@gmail.com>
2022-11-03 05:48:00 +0800

    GaugeMetricFamily

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import requests
 from flask import Flask, Response
 from prometheus_client import generate_latest, CollectorRegistry, Gauge
+from prometheus_client.core import GaugeMetricFamily
 
 app = Flask(__name__)
 
@@ -40,45 +41,8 @@ def get_metrics():
 
     ##### coin #####
 
-    btc_price = get_btc_price()
-    btc_price_metric = Gauge("btc_price", "btc price", registry=registry)
-    btc_price_metric.set(btc_price)
-
-    eth_price = get_eth_price()
-    eth_price_metric = Gauge("eth_price", "eth price", registry=registry)
-    eth_price_metric.set(eth_price)
-
-    atom_price = get_atom_price()
-    atom_price_metric = Gauge("atom_price", "atom price", registry=registry)
-    atom_price_metric.set(atom_price)
-
-    dot_price = get_dot_price()
-    dot_price_metric = Gauge("dot_price", "dot price", registry=registry)
-    dot_price_metric.set(dot_price)
-
-    ##### exchange #####
-
-    bnb_price = get_bnb_price()
-    bnb_price_metric = Gauge("bnb_price", "bnb price", registry=registry)
-    bnb_price_metric.set(bnb_price)
-
-    ftt_price = get_ftt_price()
-    ftt_price_metric = Gauge("ftt_price", "ftt price", registry=registry)
-    ftt_price_metric.set(ftt_price)
-
-    ##### defi #####
-
-    uni_price = get_uni_price()
-    uni_price_metric = Gauge("uni_price", "uni price", registry=registry)
-    uni_price_metric.set(uni_price)
-
-    aave_price = get_aave_price()
-    aave_price_metric = Gauge("aave_price", "aave price", registry=registry)
-    aave_price_metric.set(aave_price)
-
-    comp_price = get_comp_price()
-    comp_price_metric = Gauge("comp_price", "comp price", registry=registry)
-    comp_price_metric.set(comp_price)
+    coin_collector = CoinCollector()
+    registry.register(coin_collector)
 
     return registry
 
@@ -96,51 +60,32 @@ def get_gasprices():
     gasprices = resp['blockPrices'][0]['estimatedPrices']
     return gasprices
 
-def get_coin_price(coin):
-    # https://www.gate.io/api2#ticker
 
-    headers = {'content-type': 'application/json'}
-    url = "https://data.gateapi.io/api2/1/ticker/"+coin+"_usdt"
+class CoinCollector():
+    def format_metric_name(self):
+        return 'os_memory_'
 
-    resp = requests.request("GET", url).json()
-    price = float(resp['last'])
-    return price
+    def collect(self):
+        coin_list = ["btc", "eth", "atom", "dot", "bnb", "ftt", "uni", "aave", "comp"]
 
-def get_btc_price():
-    price = get_coin_price("btc")
-    return price
+        for coin in coin_list:
+            label = coin + "_price"
+            gauge = GaugeMetricFamily(label, '')
+            price = self.get_coin_price(coin)
+            gauge.add_metric(labels=[], value=price)
+            yield gauge
 
-def get_eth_price():
-    price = get_coin_price("eth")
-    return price
 
-def get_dot_price():
-    price = get_coin_price("dot")
-    return price
+    def get_coin_price(self, coin):
+        # https://www.gate.io/api2#ticker
 
-def get_atom_price():
-    price = get_coin_price("atom")
-    return price
+        headers = {'content-type': 'application/json'}
+        url = "https://data.gateapi.io/api2/1/ticker/"+coin+"_usdt"
 
-def get_bnb_price():
-    price = get_coin_price("bnb")
-    return price
+        resp = requests.request("GET", url).json()
+        price = float(resp['last'])
+        return price
 
-def get_ftt_price():
-    price = get_coin_price("ftt")
-    return price
-
-def get_uni_price():
-    price = get_coin_price("uni")
-    return price
-
-def get_aave_price():
-    price = get_coin_price("aave")
-    return price
-
-def get_comp_price():
-    price = get_coin_price("comp")
-    return price
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
